d1d9203230d8c6bf6ed2ece508de51906c231dc1
ahkamulbd <ahkamulbd@gmail.com>
2023-08-30 13:07:58 +0600

    Programme Algorithm written

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('Script for Tip Calculator');
+//console.log('Script for Tip Calculator');
+/* 
+ Program Algorithm:
+
+ 1(a) Sub-total bill calculation: Amount got from Bill input field * Number of person got from person input field
+
+ (b) Convert sub-total to float(fixed 2 point)
+
+ 2(a) Tip Calculation: Sub-total * 15% Tip
+ (b) Convert it to float (fixed 2 point)
+ (c) Show it to Tip BDT Text
+
+ 3(a) Total Bill = Sub-total Bill + Tip
+ (b) Show total on Total BDT Text
+*/
